743b526bc49518067b4d21f2511bf9a5f1046512
Dipam Donga <dipamdonga@gmail.com>
2022-01-13 11:45:31 +0530

    Update Session-3 Assignment.sql

--- Database/Session-3 Assignment.sql
+++ Database/Session-3 Assignment.sql
@@ -1,25 +1,44 @@
 
 
-/**************TCL Commands**********************/
+/*---------------------TCL Commands---------------------*/
 
-SELECT * FROM student
+SELECT * FROM Student
 
 BEGIN TRANSACTION
-INSERT INTO Student(First_name)
-VALUES('rajesh')
+UPDATE Student
+SET Student.First_name = 'User'
+WHERE Student.Age BETWEEN 20 AND 22;
+
 
 COMMIT TRANSACTION
 
 ROLLBACK TRANSACTION
 
 
-/*****************Function********************/
+/*---------------------Function---------------------*/
 SELECT * FROM Fee
 
-SELECT LOWER(First_name) FROM student
-SELECT UPPER(First_name) FROM student
 SELECT AVG(Amount_Paid) FROM Fee
-SELECT MAX(Amount_Paid) FROM Fee
-SELECT MIN(Amount_Paid) FROM Fee
 SELECT SUM(Amount_Paid) FROM Fee
 SELECT COUNT(Amount_Paid) FROM Fee
+SELECT LOWER(First_name) FROM student
+SELECT UPPER(First_name) FROM student
+SELECT MAX(Amount_Paid) FROM Fee
+SELECT MIN(Amount_Paid) FROM Fee
+
+/*---------------------User---------------------*/
+
+CREATE LOGIN dipam WITH PASSWORD = 'dipam'
+
+CREATE USER dipam FROM LOGIN dipam
+
+EXECUTE AS USER = 'dipam'
+
+SELECT * FROM fn_my_permissions ('TrainingDB','Database')
+
+GRANT SELECT ON TrainingDB TO dipam
+
+REVOKE SELECT ON TrainingDB TO dipam
+
+GRANT CONTROL ON DATABASE :: TrainingDB TO dipam
+
